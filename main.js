2c5ccaa2e8978426ec31880364f4924b86eceb80
yuribertoli <yuribertoli@yahoo.it>
2022-02-04 16:04:41 +0100

    milestone 3

--- main.js
+++ main.js
@@ -113,17 +113,63 @@ const icone = [
 	}
 ];
 
+//Mi richiamo il container del DOM
 const container = document.getElementById('container');
 
-for (i=0; i<icone.length; i++) {
+//Mi richiamo il select del DOM (il primo che trova con questa dicitura)
+const selection = document.getElementsByTagName("select")[0];
 
-    let icona = `
-                <div class="icon">
-                    <i class="${icone[i].family} ${icone[i].prefix}${icone[i].name} ${icone[i].color}"></i>
-                    <h3>${icone[i].name}</h3>
-                </div>
-    `;
+//rendo visibili al refresh della pagina tutte le icone
+iconeDOM(container, icone);
+//riporto anche la selezione sul primo indice (quindi sul valore "all")
+selection.selectedIndex = 0;
+
+//Inserisco un ascoltatore di eventi sul select 
+selection.addEventListener("change", function(){
+
+    if (selection.value == "all") { //se impostata su "all" riporto tutte le icone
+        
+        iconeDOM(container, icone);
+
+    } else {
+
+        //Creo una variabile che racchiude i type corrispondenti al .value corrente
+        let iconeFiltrate = icone.filter(oggetto => {
+
+            if (oggetto.type == selection.value) {
+                return true;
+            } 
+            return false;
+
+        });
+
+        //mostro i risultati con la variabile corrente
+        iconeDOM(container, iconeFiltrate);
+
+    }
+
+    } 
+)
+
+
+
+
+
+
+//Creo una funzione che richiama il container dove inserire gli elementi
+function iconeDOM(container, icone) {
+
+    let contenuto = "";
+
+    icone.forEach(oggetto => {
+        
+        contenuto += `   <div class="icon">
+                            <i class="${oggetto.family} ${oggetto.prefix}${oggetto.name} ${oggetto.color}"></i>
+                            <h3>${oggetto.name}</h3>
+                        </div>  `;
     
-    container.innerHTML += icona;
+    });
+
+    container.innerHTML = contenuto;
 
 }
